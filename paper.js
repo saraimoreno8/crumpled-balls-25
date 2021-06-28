e9c59d7c6e2fd88d2a1784a9396e086038888885
saraimoreno8 <75221799+saraimoreno8@users.noreply.github.com>
2021-06-27 21:36:27 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class Paper  {
+    constructor(x,y)   {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            density: 1.2,
+            friction: 0.5,
+        }
+        this.body = Bodies.circle(x,y,23,options);
+    
+        this.image = loadImage('paper.png');
+        World.add(world,this.body);
+
+    }
+
+        display()   {
+          
+            var pos = this.body.position;
+            push();
+            translate(pos.x,pos.y);
+            imageMode(CENTER);
+            image(this.image,0,0,70,70);
+            pop();
+        }
+
+}
+ 
